7de8a936e4f20cfc1c7278f9111d77b9ffaa3957
Yuvraj <yuvrajsigh1402@gmail.com>
2023-02-06 04:30:36 +0530

    Second-commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,43 @@
+import LogoTitle from '../../assets/images/fin-logo.png';
+import { Link } from 'react-router-dom'
+import './index.scss';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+
+const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['u', 'v', 'r', 'a', 'j']
+    const jobArray = ['w','e','b','','d','e','v','e','l','o','p','e','r','.']
+    
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                <br />
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
+                <br />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22} />
+                </h1>
+                <h2>Frontend Developer / Cybersec Enthusiast / Astrophile</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Home
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,39 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
-import LogoY from '../../assets/images/yuvraj-logo.png'
+import { Link, NavLink } from 'react-router-dom'
+import LogoY from '../../assets/images/yu-logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
-const Sidebar = () => {
+const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoY} alt="logo" />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/yuvraj-singh-0992591b7'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/Crunchygrunt'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
-}
+)
 
 export default Sidebar
