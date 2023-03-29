fa675b6f230a079c15142c6596796c86af5804ec
Yuvraj <yuvrajsigh1402@gmail.com>
2023-03-30 01:13:07 +0530

    contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,35 @@
 import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
-const Contact = () => 'Hello'
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+    return(
+        <>
+        <div className='container contact-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={['C','o','n','t','a','c','t',' ','m','e']} 
+                    idx={15}
+                    />
+                </h1>
+                <p>
+                    d
+                </p>
+            </div>
+        </div>
+        <Loader type='pacman' />
+        </>
+    )
+}
 
 export default Contact
