1f5081d7e7fc49b9f15f93f9c530200af3ec1c05
Yuvraj <yuvrajsigh1402@gmail.com>
2023-03-30 03:40:25 +0530

    Contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,39 @@
 import './index.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-
+    const refForm = useRef()
     useEffect(() => {
         setTimeout(() => {
             return setLetterClass('text-animate-hover')
         }, 3000)
     }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'service_ft2t1ax',
+                'template_7rn5yqg',
+                refForm.current,
+                'H6vWmRt7GdrvamnA4'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again')
+                }
+            )
+    }
+
     return(
         <>
         <div className='container contact-page'>
@@ -23,8 +46,41 @@ const Contact = () => {
                     />
                 </h1>
                 <p>
-                    d
+                    I am interested in inter opportunities - especially ambitious and 
+                    creative projects relating to new technologies that I can learn and 
+                    demonstrate and work with highly motivated and experienced teams.
+                    For any questions or doubts, you can contact me using the below form.
                 </p>
+                <div className='contact-form'>
+                    <form ref={refForm} onSubmit={sendEmail}>
+                        <ul>
+                            <li className='half'>
+                                <input type='text' name='name' placeholder='Name' required />
+                            </li>
+                            <li className='half'>
+                                <input type='email' name='email' placeholder='Email' required />
+                            </li>
+                            <li>
+                                <input placeholder='Subject' type='text' name='subject' required />
+                            </li>
+                            <li>
+                                <textarea placeholder='Message' name='message' required></textarea>
+                            </li>
+                            <li>
+                                <input type='submit' className='flat-button' value='SEND' />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+            <div className='info-map'>
+                Yuvraj Singh,
+                <br />
+                India,
+                <br />
+                SRM University <br />
+                Tamil Nadu <br />
+                <span>yuvrajsigh1402@gmail.com</span>
             </div>
         </div>
         <Loader type='pacman' />
